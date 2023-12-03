efaa4e150e6e7a638e863f0bd68b816e88acc118
John Doe <nguyenhoangthe93@gmail.com>
2023-12-03 17:44:50 +0700

    update

---
+++ JSX/JSX3/src/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="containter bg-secondary py-2 text-white">
+            Footer
+        </div>
+    )
+}
+export default Footer;
